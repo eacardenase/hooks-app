fbdd504cbdcf9345eadb4aeb5cce6b961b1cc3f1
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-11-16 11:12:45 -0500

    Using useFetch alongside useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,16 +1,40 @@
 import { useFetch } from '../hooks/useFetch';
+import { useCounter } from '../hooks/useCounter';
 
 export const MultipleCustomHooks = () => {
+    const { counter, incrementValue } = useCounter(1);
+
     const { data, isLoading, hasError } = useFetch(
-        'https://www.breakingbadapi.com/api/quotes/1'
+        `https://www.breakingbadapi.com/api/quotes/${counter}`
     );
 
-    console.log({ data, isLoading, hasError });
+    const { author, quote } = !!data && data[0];
+
+    // if (isLoading) {
+    //     return <div className="alert alert-info text-center">Loading...</div>;
+    // }
 
     return (
         <>
             <h1>Braking Bad Quotes</h1>
             <hr />
+
+            {isLoading ? (
+                <div className="alert alert-info text-center">Loading...</div>
+            ) : (
+                <blockquote className="blockquote text-end">
+                    <p className="mb-1">{quote}</p>
+                    <footer className="blockquote-footer mt-1">{author}</footer>
+                </blockquote>
+            )}
+
+            <button
+                className="btn btn-primary"
+                onClick={() => incrementValue()}
+                disabled={isLoading}
+            >
+                Next quote
+            </button>
         </>
     );
 };
