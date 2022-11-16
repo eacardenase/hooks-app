96e97106b5d1c34817d0f3762f362f623630decd
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-11-16 18:03:48 -0500

    useLayoutEffect hook

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,12 +1,39 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
 import PropTypes from 'prop-types';
 
 export const Quote = ({ author, quote }) => {
+    const quoteRef = useRef();
+    const [boxSize, setBoxSize] = useState({
+        width: 0,
+        height: 0,
+    });
+
+    useLayoutEffect(() => {
+        const { width, height } = quoteRef.current.getBoundingClientRect();
+
+        setBoxSize({
+            width,
+            height,
+        });
+    }, [quote]);
+
     return (
         <>
-            <blockquote className="blockquote text-end">
-                <p className="mb-1">{quote}</p>
+            <blockquote
+                className="blockquote text-end"
+                // style={{
+                //     display: 'flex',
+                // }}
+            >
+                <p className="mb-1" ref={quoteRef}>
+                    {quote}
+                </p>
                 <footer className="blockquote-footer mt-1">{author}</footer>
             </blockquote>
+
+            <code>{JSON.stringify(boxSize)}</code>
+            <hr />
         </>
     );
 };
--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -3,8 +3,9 @@
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-useRef/FocusScreen';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
 
 export const HooksApp = () => {
-    return <FocusScreen />;
+    return <Layout />;
 };
