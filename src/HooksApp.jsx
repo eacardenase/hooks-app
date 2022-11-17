5f7bb051a7abf4861dd1742873ba47dda688f9cb
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-11-16 19:13:03 -0500

    useMemo hook

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -5,8 +5,9 @@
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
-import { Memorize } from './06-memos/Memorize';
+// import { Memorize } from './06-memos/Memorize';
+import { MemoHook } from './06-memos/MemoHook';
 
 export const HooksApp = () => {
-    return <Memorize />;
+    return <MemoHook />;
 };
