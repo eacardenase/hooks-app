c0a5ac8a9d7ca84f0d7d640b29c9ba48c04149fa
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-11-21 16:22:20 -0500

    Adding Context and Provider to <MainApp />

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,13 +1,14 @@
-import { Navigate, Route, Routes, Link } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { HomePage } from './HomePage';
 import { AboutPage } from './AboutPage';
 import { LoginPage } from './LoginPage';
 import { Navbar } from './Navbar';
+import { UserProvider } from './context/UserProvider';
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             <Navbar />
 
             <hr />
@@ -20,6 +21,6 @@ export const MainApp = () => {
                 {/* <Route path="/*" element={<AboutPage />} /> */}
                 <Route path="/*" element={<Navigate to="/about" />} />
             </Routes>
-        </>
+        </UserProvider>
     );
 };
---
+++ src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,20 @@
+import { UserContext } from './UserContext';
+
+const user = {
+    id: 123,
+    name: 'Edwin Cardenas',
+    email: 'eacardenase@gmail.com',
+};
+
+export const UserProvider = ({ children }) => {
+    return (
+        <UserContext.Provider
+            value={{
+                hola: 'Mundo',
+                user,
+            }}
+        >
+            {children}
+        </UserContext.Provider>
+    );
+};
